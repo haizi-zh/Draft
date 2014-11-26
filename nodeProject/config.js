986913d23c159bfeb2bc4ae5d297026458f877ad
ck <chenkaiC4@gmail.com>
2014-11-26 15:21:11 +0800

    little fix

--- nodeProject/config.js
+++ nodeProject/config.js
@@ -1,7 +1,5 @@
 var config = {
-    //db: 'mongodb://api.lvxingpai.cn:27017/geo',
-    dbgeo: 'mongodb://api.lvxingpai.cn:27017/geo',
-    dbimagestore: 'mongodb://api.lvxingpai.cn:27017/imagestore'
+    // db connection;
 };
 
 
