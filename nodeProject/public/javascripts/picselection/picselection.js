1f65c9f1baa193dd63d2401184aff4e3bd504fed
ck <chenkaiC4@gmail.com>
2014-12-05 11:36:31 +0800

    add search function

--- nodeProject/public/javascripts/picselection/picselection.js
+++ nodeProject/public/javascripts/picselection/picselection.js
@@ -8,7 +8,7 @@ $(function(){
             pics = document.forms[0].pic,
             sum = 0,
             index = []
-        
+
 	// 筛选索引
         if(!(pics[0] == null || pics[0] == undefined)) {
             for (var i = 0;i < pics.length; ++i){
@@ -73,6 +73,7 @@ $(function(){
 
 
     $("#next").on('click',function(){
+        $('#J_search_text').val('');
         if ($('#submit').attr('data-submit') == "true"){
             $.ajax({
                 url : requestUrl,
@@ -205,7 +206,7 @@ $(function(){
                                         right = x2 * ow / cw,
                                         bottom = y2 * oh / ch,
                                         cropHint= {};
-                                    
+
                                     left = parseInt(left);
                                     left = (left % 2) ? (left + 1) : left;
                                     right = parseInt(right);
@@ -223,10 +224,10 @@ $(function(){
                                     };
                                     cropHints[index] = cropHint;
                                     alert("裁剪成功!");
-                                    $('.sd_layer').trigger('click');   
+                                    $('.sd_layer').trigger('click');
                                 }
                             });
-                        });                        
+                        });
                     }else{
                         alert('Failed to get data!');
                     }
@@ -239,6 +240,199 @@ $(function(){
             alert('请先提交改动!');
         }
     });
+
+    // 查找
+    $('#J_confirmSearch').click(function(){
+        var search_text_row = $('#J_search_text').val(),
+            search_text = $.trim(search_text_row),
+            url = '/picselection/search';
+
+
+        var lastInput = $(this).attr('data-last');
+        if(search_text == ''){
+            $(this).attr('data-last', '')
+            alert("请输入城市或景点名字");
+            return;
+        }
+
+        if(lastInput == search_text) {
+            return;
+        }else{
+            $('#J_confirmSearch').attr('data-clicked', 'false');
+        }
+
+        var clickFlag = $(this).attr('data-clicked');
+        if('false' == clickFlag){
+            $('#J_confirmSearch').attr('data-clicked', 'true');
+            $(this).attr('data-last', search_text)
+            $.ajax({
+                url : url,
+                type: "GET",
+                data: {search_text: search_text},
+                success : function (data) {
+                    console.log(data);
+
+                    if (data.code == 0){
+                        imgBox = null;
+                        imgBox = data.images;
+                        /*modify the DOM*/
+                        $('form').remove();
+                        $('.city').children('b').text(data.name);
+                        $('.city').attr('data-id' , data.poiId);
+                        $('.imgCnt').children('b').text(data.images.length);
+                        $('.restCity').children('b').text(data.rest);
+                        $('.doneCity').children('b').text(data.done);
+
+                        /*add the DOM*/
+                        $('.hd').after('<form></form>');
+                        var imgBlock;
+                        for(var i in imgBox){
+                            imgBlock =
+                                '<div class="pics">' +
+                                    '<a class="img" data-id="' + i + '">' +
+                                        '<img src="' + imgBox[i].url + '?imageView2/1/w/225/h/150" width="225px" height="150px"/>' +
+                                    '</a>' +
+                                    '<input class="pic" name="pic" type="checkbox" value="' + i + '"/>' +
+                                '</div>';
+                            $('form').append(imgBlock);
+                            cropHints[i] = {
+                                "top": 0,
+                                "left": 0,
+                                "bottom": 0,
+                                "right": 0
+                            };
+                        }
+
+                        /*add the imgLayer*/
+                        $('a.img').on('click', function(){
+                            $('.sd_layer').css("display","block");
+                            var index = $(this).attr("data-id");
+                                img = '<img id="imageCrop" src="' + imgBox[index].url + '?imageView2/2/w/800/h/600/interlace/1"/>';
+                            $('.img_layer .img_can').append(img);
+                            $('.img_layer').css("display","block");
+                            $('#ow').val(imgBox[index].w);
+                            $('#oh').val(imgBox[index].h);
+
+                            var selectFlag = false;//if cut ,the flag change to true
+                            function showCoords(c){
+                                selectFlag = true;
+                                $('#x1').val(c.x);
+                                $('#y1').val(c.y);
+                                $('#x2').val(c.x2);
+                                $('#y2').val(c.y2);
+                                $('#w').val(c.w);
+                                $('#h').val(c.h);
+                                $('#cw').val(jcrop_api.getBounds()[0]);
+                                $('#ch').val(jcrop_api.getBounds()[1]);
+                            };
+
+                            function clearCoords(){
+                                $('#x1').val('');
+                                $('#y1').val('');
+                                $('#x2').val('');
+                                $('#y2').val('');
+                                $('#w').val('');
+                                $('#h').val('');
+                            };
+
+                            $('#imageCrop').Jcrop({
+                                onChange: showCoords,
+                                onSelect: showCoords,
+                                onRelease: clearCoords
+                            },function(){
+                                jcrop_api = this;
+                            });
+
+                            $('#coords').on('change','input',function(e){
+                                var x1 = $('#x1').val(),
+                                    x2 = $('#x2').val(),
+                                    y1 = $('#y1').val(),
+                                    y2 = $('#y2').val();
+                                jcrop_api.setSelect([x1,y1,x2,y2]);
+                            });
+
+                            /*close sdLayer*/
+                            $('.sd_layer').on('click', function(){
+                                $(this).css("display","none");
+                                $('.img_layer').css("display","none");
+                                $('#coords input').val('');
+                                $('.img_layer .img_can').empty();
+                            });
+                            $('#close').on('click',function(){
+                                $('.sd_layer').trigger('click');
+                            })
+
+                            $('#imgSubmit').off('click');
+                            $('#imgSubmit').on('click',function(){
+                                if (!selectFlag){
+                                    if(confirm("并未对该图进行裁剪,是否保持当前尺寸?")){
+                                        var ow = $('#ow').val(),
+                                            oh = $('#oh').val(),
+                                            cropHint;
+                                        ow = (ow % 2) ? (ow - 1) : ow;
+                                        oh = (oh % 2) ? (oh - 1) : oh;
+                                        cropHint = {
+                                            "top": 0,
+                                            "left": 0,
+                                            "bottom": oh,
+                                            "right": ow
+                                        };
+                                        cropHints[index] = cropHint;
+                                        alert("裁剪成功!");
+                                        $('.sd_layer').trigger('click');
+                                    }else{
+
+                                    }
+                                }else{
+                                    var x1 = $('#x1').val(),
+                                        x2 = $('#x2').val(),
+                                        y1 = $('#y1').val(),
+                                        y2 = $('#y2').val(),
+                                        cw = $('#cw').val(),
+                                        ch = $('#ch').val(),
+                                        ow = $('#ow').val(),
+                                        oh = $('#oh').val(),
+                                        left = x1 * ow / cw,
+                                        top = y1 * oh / ch,
+                                        right = x2 * ow / cw,
+                                        bottom = y2 * oh / ch,
+                                        cropHint= {};
+
+                                    left = parseInt(left);
+                                    left = (left % 2) ? (left + 1) : left;
+                                    right = parseInt(right);
+                                    right = (right % 2) ? (right - 1) : right;
+                                    top = parseInt(top);
+                                    top = (top % 2) ? (top + 1) : top;
+                                    bottom = parseInt(bottom);
+                                    bottom = (bottom % 2) ? (bottom - 1) : bottom;
+
+                                    cropHint = {
+                                        "top": top,
+                                        "left": left,
+                                        "bottom": bottom,
+                                        "right": right
+                                    };
+                                    cropHints[index] = cropHint;
+                                    alert("裁剪成功!");
+                                    $('.sd_layer').trigger('click');
+                                }
+                            });
+                        });
+
+                    }else{
+                        alert('数据查询失败，无相关景点或城市，请确认');
+                    }
+                },
+                error : function () {
+                    $('#J_confirmSearch').attr('data-clicked', 'false');
+                    alert('网络太卡 或者 代码罢工，赶紧呼叫程序猿');
+                }
+            })
+        }
+    })
+
+
     $(window).load(function(){
         $("#next").trigger('click');
         var wHeight = $(window).height(),
